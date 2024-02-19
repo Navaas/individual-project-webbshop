3b05374d8cd950c0e2d11094c5ccb884f7605dfa
Michaela <mi-and@outlook.com>
2024-02-19 21:07:01 +0100

    Clean up code and create component: footer, header and homepage

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>Hello from Header</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/page/Homepage.tsx
@@ -0,0 +1,9 @@
+function Homepage() {
+  return (
+    <div>
+      <h2>Hello from HomePage</h2>
+    </div>
+  );
+}
+
+export default Homepage;
