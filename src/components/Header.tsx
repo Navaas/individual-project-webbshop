37571d4af6f733659e62b74c2436cfc082d31261
Michaela <mi-and@outlook.com>
2024-02-19 22:07:57 +0100

    Add styled-components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,25 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: black;
+  color: white;
+  width: 100px;
+`;
+const HeaderDiv = styled.header`
+  background: grey;
+  display: flex;
+  justify-content: space-between;
+`;
 
 function Header() {
   return (
-    <header>
+    <HeaderDiv>
       <Link to="/">
-        <button>Hem</button>
+        <Button>Hem</Button>
       </Link>
       <h1>Hello from Header</h1>
-    </header>
+    </HeaderDiv>
   );
 }
 
