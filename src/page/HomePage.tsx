b9d9a992c34802dbac55d3b62838d0cf231449b5
Michaela <mi-and@outlook.com>
2024-02-20 12:25:50 +0100

    Homepage desktop and mobile working

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,18 +1,79 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import background from "../image/background.jpg";
+
+const Container = styled.div`
+  background: url(${background}) no-repeat center center fixed;
+  background-size: cover;
+  height: 65vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const BoxContainer = styled.div`
+  display: flex;
+  column-gap: 30px;
+  position: absolute;
+  bottom: 150px;
+  flex-direction: row;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    position: static;
+    bottom: auto;
+  }
+`;
+
+const Box = styled.div`
+  background: white;
+  border: solid 1px black;
+  width: 200px;
+  height: 150px;
+  z-index: 1;
+  @media (max-width: 768px) {
+    width: 150px;
+    height: 100px;
+  }
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  z-index: 1;
+`;
 
 function Homepage() {
   return (
-    <div>
-      <h1>HomePage</h1>
+    <Container>
+      <TextWrapper>
+        <h1>HomePage</h1>
+      </TextWrapper>
       {/* {mockedProducts.map((product) => (
         <Link key={product.id} to={"/productPage/" + product.id}>
           {product.title}
         </Link>
       ))} */}
-      <Link to="productPage">
-        <button>Till produkter</button>
-      </Link>
-    </div>
+
+      <BoxContainer>
+        <Box>
+          {" "}
+          <Link to="productPage">
+            <button>Till produkter</button>
+          </Link>
+        </Box>
+        <Box>
+          {" "}
+          <Link to="productPage">
+            <button>Till produkter</button>
+          </Link>
+        </Box>
+        <Box>
+          {" "}
+          <Link to="productPage">
+            <button>Till produkter</button>
+          </Link>
+        </Box>
+      </BoxContainer>
+    </Container>
   );
 }
 
