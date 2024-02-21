6d9164e592463720f81e09c9ca0dcd09be1eb660
Michaela <mi-and@outlook.com>
2024-02-21 08:54:39 +0100

    Add images and render on homepage

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,87 +1,92 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import background from "../image/background.jpg";
+import background from "../image/background1.jpeg";
+import image from "../image/start.jpg";
 
 const Container = styled.div`
-  background: url(${background}) no-repeat center center fixed;
-  background-size: cover;
-  height: 65vh;
   display: flex;
   align-items: center;
   justify-content: center;
-  @media (max-width: 768px) {
-    flex-direction: column;
-    min-height: 100vh;
-  }
+  flex-direction: column;
+  height: 100vh;
 `;
 
-const BoxContainer = styled.div`
-  display: flex;
-  column-gap: 30px;
-  position: absolute;
-  bottom: 150px;
-  flex-direction: row;
-  @media (max-width: 768px) {
-    flex-direction: column;
-    position: static;
-    bottom: auto;
-  }
-`;
-
-const Box = styled.div`
-  background: white;
-  border: solid 1px black;
-  width: 200px;
-  height: 150px;
-  z-index: 1;
-  @media (max-width: 768px) {
-    width: 150px;
-    height: 100px;
-    margin-bottom: 20px;
-  }
+const TextContainer = styled.div`
+  background-image: url(${background});
+  background-size: cover;
+  background-position: center;
+  flex-direction: column;
+  color: black;
+  align-items: center;
+  border: black solid 1px;
+  flex: 1;
+  width: 100%;
 `;
 
 const TextWrapper = styled.div`
+  background: white;
+  color: black;
+`;
+
+const ButtonContainer = styled.div`
+  color: white;
+  align-items: center;
+  flex-direction: row;
+  border: red solid 1px;
+  flex: 1;
+  width: 100%;
   display: flex;
-  z-index: 1;
-  @media (max-width: 768px) {
-    width: 150px;
-    height: 100px;
-  }
+  justify-content: center;
+`;
+
+const ImageWrapper = styled.div`
+  width: 300px;
+  height: 200px;
+`;
+
+const StyledImage = styled.img`
+  width: 20%;
+  height: 40%;
+  object-fit: cover; /* Anpassa storleken på bilden utan att förvränga den */
+  border-top-left-radius: 999px; /* Runda bara de övre vänstra hörnen */
+  border-top-right-radius: 999px;
 `;
 
 function Homepage() {
   return (
     <Container>
-      <TextWrapper>
+      <TextContainer>
         <h1>HomePage</h1>
-      </TextWrapper>
-      {/* {mockedProducts.map((product) => (
-        <Link key={product.id} to={"/productPage/" + product.id}>
-          {product.title}
-        </Link>
-      ))} */}
-
-      <BoxContainer>
-        <Box>
-          {" "}
-          <Link to="productPage">
-            <button>Till produkter</button>
-          </Link>
-        </Box>
-        <Box>
-          {" "}
-          <Link to="productPage">
-            <button>Till produkter</button>
-          </Link>
-        </Box>
-        <Box>
-          {" "}
-          <Link to="productPage">
-            <button>Till produkter</button>
-          </Link>
-        </Box>
-      </BoxContainer>
+        <TextWrapper>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta id,
+            ab illo dicta cumque ipsa unde culpa quae mollitia dolorum error
+            eius excepturi dolorem, iusto libero quaerat praesentium illum? At.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            iste eaque deserunt neque obcaecati aliquam ea dolores non optio
+            recusandae unde fugit ullam quaerat nulla placeat in, amet, quod
+            veniam.
+          </p>
+        </TextWrapper>
+      </TextContainer>
+      <ButtonContainer>
+        <ImageWrapper>
+          <StyledImage src={image} alt="My Image" />
+          <StyledImage src={image} alt="My Image" />
+          <StyledImage src={image} alt="My Image" />
+          <div>
+            <Link to="productPage">
+              <button>Till produkter</button>
+            </Link>
+            <Link to="productPage">
+              <button>Till produkter</button>
+            </Link>
+            <Link to="productPage">
+              <button>Till produkter</button>
+            </Link>
+          </div>
+        </ImageWrapper>
+      </ButtonContainer>
     </Container>
   );
 }
