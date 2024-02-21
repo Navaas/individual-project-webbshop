08a70193fb4ab67826bd5138348af57c2748bbe9
Michaela <mi-and@outlook.com>
2024-02-21 17:20:10 +0100

    Add mor mocked data

--- src/data/mockedData.ts
+++ src/data/mockedData.ts
@@ -26,4 +26,47 @@ export const mockedProducts: Product[] = [
     price: 99,
     image: test,
   },
+  {
+    id: "1",
+    title: "Tulpan lökar",
+    price: 99,
+    image: test,
+  },
+  {
+    id: "2",
+    title: "Astrid Lindgren rosenbuske",
+    price: 345,
+    image: test,
+  },
+  {
+    id: "3",
+    title: "Dalior frön",
+    price: 99,
+    image: test,
+  },
+
+  {
+    id: "3",
+    title: "Dalior frön",
+    price: 99,
+    image: test,
+  },
+  {
+    id: "1",
+    title: "Tulpan lökar",
+    price: 99,
+    image: test,
+  },
+  {
+    id: "2",
+    title: "Astrid Lindgren rosenbuske",
+    price: 345,
+    image: test,
+  },
+  {
+    id: "3",
+    title: "Dalior frön",
+    price: 99,
+    image: test,
+  },
 ];
