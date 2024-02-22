67d70aab44bc3c09bb81cbfe9564e60265b9a36b
Michaela <mi-and@outlook.com>
2024-02-22 16:25:02 +0100

    Add new mocked data

--- src/data/mockedData.ts
+++ src/data/mockedData.ts
@@ -1,5 +1,3 @@
-import test from "../image/test.png";
-
 export interface Product {
   id: string;
   title: string;
@@ -11,82 +9,92 @@ export interface Product {
 export const mockedProducts: Product[] = [
   {
     id: "1",
-    title: "Tulpan lökar",
+    title: "Two in the lake",
     price: 99,
-    image: test,
+    image:
+      "https://images.desenio.com/zoom/10570_1.jpg?_gl=1*16pn5pg*_ga*ODY4MDk3NTgwLjE3MDg2MDY1NDE.*_ga_GH3FS7X5TH*MTcwODYxMzQ4MS4zLjEuMTcwODYxNDE3Ni4wLjAuMA..",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ut laboriosam quasi ratione ipsam debitis doloremque labore nulla ipsum voluptas laudantium adipisci, libero temporibus qui ex saepe tempora cupiditate odit!",
   },
   {
     id: "2",
-    title: "Astrid Lindgren rosenbuske",
+    title: "Two in the lake 1",
     price: 345,
-    image: test,
+    image:
+      "https://images.desenio.com/zoom/10570_1.jpg?_gl=1*16pn5pg*_ga*ODY4MDk3NTgwLjE3MDg2MDY1NDE.*_ga_GH3FS7X5TH*MTcwODYxMzQ4MS4zLjEuMTcwODYxNDE3Ni4wLjAuMA..",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ut laboriosam quasi ratione ipsam debitis doloremque labore nulla ipsum voluptas laudantium adipisci, libero temporibus qui ex saepe tempora cupiditate odit!",
   },
   {
     id: "3",
-    title: "Dalior frön",
+    title: "Two in the lake 2",
     price: 99,
-    image: test,
+    image:
+      "https://images.desenio.com/zoom/10570_1.jpg?_gl=1*16pn5pg*_ga*ODY4MDk3NTgwLjE3MDg2MDY1NDE.*_ga_GH3FS7X5TH*MTcwODYxMzQ4MS4zLjEuMTcwODYxNDE3Ni4wLjAuMA..",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ut laboriosam quasi ratione ipsam debitis doloremque labore nulla ipsum voluptas laudantium adipisci, libero temporibus qui ex saepe tempora cupiditate odit!",
   },
   {
     id: "4",
-    title: "Tulpan lökar",
+    title: "Two in the lake 3",
     price: 99,
-    image: test,
+    image:
+      "https://images.desenio.com/zoom/10570_1.jpg?_gl=1*16pn5pg*_ga*ODY4MDk3NTgwLjE3MDg2MDY1NDE.*_ga_GH3FS7X5TH*MTcwODYxMzQ4MS4zLjEuMTcwODYxNDE3Ni4wLjAuMA..",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ut laboriosam quasi ratione ipsam debitis doloremque labore nulla ipsum voluptas laudantium adipisci, libero temporibus qui ex saepe tempora cupiditate odit!",
   },
   {
     id: "5",
-    title: "Astrid Lindgren rosenbuske",
+    title: "Two in the lake 4",
     price: 345,
-    image: test,
+    image:
+      "https://images.desenio.com/zoom/10570_1.jpg?_gl=1*16pn5pg*_ga*ODY4MDk3NTgwLjE3MDg2MDY1NDE.*_ga_GH3FS7X5TH*MTcwODYxMzQ4MS4zLjEuMTcwODYxNDE3Ni4wLjAuMA..",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ut laboriosam quasi ratione ipsam debitis doloremque labore nulla ipsum voluptas laudantium adipisci, libero temporibus qui ex saepe tempora cupiditate odit!",
   },
   {
     id: "6",
-    title: "Dalior frön",
+    title: "Two in the lake 5",
     price: 99,
-    image: test,
+    image:
+      "https://images.desenio.com/zoom/10570_1.jpg?_gl=1*16pn5pg*_ga*ODY4MDk3NTgwLjE3MDg2MDY1NDE.*_ga_GH3FS7X5TH*MTcwODYxMzQ4MS4zLjEuMTcwODYxNDE3Ni4wLjAuMA..",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ut laboriosam quasi ratione ipsam debitis doloremque labore nulla ipsum voluptas laudantium adipisci, libero temporibus qui ex saepe tempora cupiditate odit!",
   },
 
   {
     id: "7",
-    title: "Dalior frön",
+    title: "Two in the lake 6",
     price: 99,
-    image: test,
+    image:
+      "https://images.desenio.com/zoom/10570_1.jpg?_gl=1*16pn5pg*_ga*ODY4MDk3NTgwLjE3MDg2MDY1NDE.*_ga_GH3FS7X5TH*MTcwODYxMzQ4MS4zLjEuMTcwODYxNDE3Ni4wLjAuMA..",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ut laboriosam quasi ratione ipsam debitis doloremque labore nulla ipsum voluptas laudantium adipisci, libero temporibus qui ex saepe tempora cupiditate odit!",
   },
   {
     id: "8",
-    title: "Tulpan lökar",
+    title: "Two in the lake 7",
     price: 99,
-    image: test,
+    image:
+      "https://images.desenio.com/zoom/10570_1.jpg?_gl=1*16pn5pg*_ga*ODY4MDk3NTgwLjE3MDg2MDY1NDE.*_ga_GH3FS7X5TH*MTcwODYxMzQ4MS4zLjEuMTcwODYxNDE3Ni4wLjAuMA..",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ut laboriosam quasi ratione ipsam debitis doloremque labore nulla ipsum voluptas laudantium adipisci, libero temporibus qui ex saepe tempora cupiditate odit!",
   },
   {
     id: "9",
-    title: "Astrid Lindgren rosenbuske",
+    title: "Two in the lake 8",
     price: 345,
-    image: test,
+    image:
+      "https://images.desenio.com/zoom/10570_1.jpg?_gl=1*16pn5pg*_ga*ODY4MDk3NTgwLjE3MDg2MDY1NDE.*_ga_GH3FS7X5TH*MTcwODYxMzQ4MS4zLjEuMTcwODYxNDE3Ni4wLjAuMA..",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ut laboriosam quasi ratione ipsam debitis doloremque labore nulla ipsum voluptas laudantium adipisci, libero temporibus qui ex saepe tempora cupiditate odit!",
   },
   {
     id: "10",
-    title: "Dalior frön",
+    title: "Two in the lake 9",
     price: 99,
-    image: test,
+    image:
+      "https://images.desenio.com/zoom/10570_1.jpg?_gl=1*16pn5pg*_ga*ODY4MDk3NTgwLjE3MDg2MDY1NDE.*_ga_GH3FS7X5TH*MTcwODYxMzQ4MS4zLjEuMTcwODYxNDE3Ni4wLjAuMA..",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ut laboriosam quasi ratione ipsam debitis doloremque labore nulla ipsum voluptas laudantium adipisci, libero temporibus qui ex saepe tempora cupiditate odit!",
   },
