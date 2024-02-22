712c2747a4f6aba4989db4b7863b9076b993d2d4
Michaela <mi-and@outlook.com>
2024-02-22 18:48:53 +0100

    Style desktop and mobile

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -6,9 +6,14 @@ const Container = styled.div`
   background: #f1edea;
   display: flex;
   align-items: center;
-  justify-content: center;
   flex-direction: column;
   height: 120vh;
+  @media (max-width: 768px) {
+    height: 155vh;
+  }
+  @media (max-width: 480px) {
+    height: 200vh;
+  }
 `;
 
 const TextContainer = styled.div`
@@ -20,22 +25,40 @@ const TextContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    margin: 0;
+  }
 `;
 
 const Text = styled.div`
   /* border: solid 1px black; */
   width: 100%;
+  margin-top: 30px;
 `;
 
 const Image = styled.div`
   /* border: solid 1px red; */
   width: 100%;
+  @media (max-width: 768px) {
+    margin-top: 50px;
+    margin-bottom: 50px;
+  }
+  @media (max-width: 480px) {
+    width: 100%;
+  }
 `;
 
 const StyledImage = styled.img`
   width: 50%;
   border-top-left-radius: 999px;
   border-top-right-radius: 999px;
+  @media (max-width: 768px) {
+    width: 40%;
+  }
+  @media (max-width: 480px) {
+    width: 60%;
+  }
 `;
 
 const StyledImageHomeSide = styled.img`
@@ -49,6 +72,9 @@ const ProductButton = styled.button`
   width: 150px;
   padding: 10px;
   text-transform: uppercase;
+  @media (max-width: 480px) {
+    padding: 7px;
+  }
 `;
 
 const ImageContainer = styled.div`
@@ -61,11 +87,30 @@ const ImageContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    margin: 0;
+  }
 `;
 
 const ProductImage = styled.div`
   margin-top: 50px;
+  @media (max-width: 768px) {
+    margin-top: 30px;
+  }
 `;
+
+// @media (max-width: 768px) {
+//   grid-template-columns: repeat(2, 1fr);
+//   justify-items: center;
+// }
+
+// @media (max-width: 480px) {
+//   grid-template-columns: repeat(
+//     1,
+//     1fr
+//   ); /* Vid mycket små skärmar, en kolumn */
+// }
 
 function Homepage() {
   return (
@@ -90,12 +135,12 @@ function Homepage() {
 
       <ImageContainer>
         <ProductImage>
-          <p>
+          <span>
             <StyledImage
               src="https://images.desenio.com/zoom/2562_2-23931.jpg?_gl=1*4esxnv*_ga*ODY4MDk3NTgwLjE3MDg2MDY1NDE.*_ga_GH3FS7X5TH*MTcwODYwNjU0MS4xLjEuMTcwODYwODM1My4wLjAuMA.."
               alt="My Image"
             />
-          </p>
+          </span>
           <div>
             <Link to={"/ProductPage"}>
               <ProductButton>Shoppa nu</ProductButton>
