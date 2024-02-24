fe99a07b46aa529956b81bb85ce718fa0e124af0
Michaela <mi-and@outlook.com>
2024-02-24 12:30:02 +0100

    Add icons i footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,21 +4,75 @@ const FooterContainer = styled.div`
   background: #ede4dd;
   color: black;
   bottom: 0;
-  justify-content: center;
-  align-items: center;
-  display: flex;
-  height: 3.5rem;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
   width: 100%;
   text-align: center;
+  justify-content: center;
+  align-content: center;
   z-index: 999;
+  @media (max-width: 768px) {
+    /* grid-template-columns: repeat(2, 1fr); */
+  }
+
+  @media (max-width: 480px) {
+    grid-template-columns: repeat(
+      1,
+      1fr
+    ); /* Vid mycket små skärmar, en kolumn */
+  }
 `;
 
+const FooterContent = styled.div`
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 10px;
+`;
+
+const Item1 = styled.div`
+  padding: 0;
+`;
+
+const Item2 = styled.div``;
+
+const Item3 = styled.div``;
+
+const ItemIcons = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-size: 40px;
+`;
 function Footer() {
   return (
     <FooterContainer>
-      <div>
-        <p style={{ margin: 0 }}>Copyright by: Michaela Andreasson hejhej</p>
-      </div>
+      <FooterContent>
+        <h3>Hjälp & Support</h3>
+        <Item1>
+          <p>Kundservice</p>
+          <p>Leverans</p>
+          <p>Return</p>
+          <p>Betalning</p>
+        </Item1>
+      </FooterContent>
+      <FooterContent>
+        <Item2>
+          <p style={{ margin: 0 }}>Copyright by: Michaela Andreasson 2024 ©</p>
+        </Item2>
+      </FooterContent>
+      <FooterContent>
+        <Item3>
+          <span>Följ oss</span>
+          <ItemIcons>
+            <i className="ri-facebook-circle-fill"></i>
+            <i className="ri-instagram-fill"></i>
+            <i className="ri-tiktok-fill"></i>
+          </ItemIcons>
+        </Item3>
+      </FooterContent>
     </FooterContainer>
   );
 }
