f7dd88572d295be4d989021a24825dd06755239a
Michaela <mi-and@outlook.com>
2024-02-26 18:50:31 +0100

    Add animation

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import image1 from "../image/image.png";
 
 const Container = styled.div`
@@ -35,6 +35,19 @@ const Text = styled.div`
   /* border: solid 1px black; */
   width: 100%;
   margin-top: 30px;
+  left: 0;
+  transform: translate(-100%, -50%);
+  opacity: 0;
+  animation: ${keyframes`
+    0% {
+      opacity: 0;
+      transform: translateX(-100%);
+    }
+    100% {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  `} 2s ease forwards;
 `;
 
 const Image = styled.div`
@@ -53,6 +66,12 @@ const StyledImage = styled.img`
   width: 50%;
   border-top-left-radius: 999px;
   border-top-right-radius: 999px;
+  transition: filter 0.5s ease;
+  &:hover {
+    opacity: 0.5; /* Ändra opacity till 0.5 vid hover */
+    transform: scale(1.1); /* Skala upp bilden till 110% av sin storlek */
+  }
+
   @media (max-width: 768px) {
     width: 40%;
   }
@@ -66,12 +85,36 @@ const StyledImageHomeSide = styled.img`
   border-top-left-radius: 999px;
   border-top-right-radius: 999px;
   background: #f1edea;
+  transition: filter 0.5s ease;
+  transform: translate(-100%, -50%);
+  opacity: 0;
+  animation: ${keyframes`
+    0% {
+      opacity: 0;
+      transform: translateY(-100%);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  `} 2s ease forwards;
+
+  &:hover {
+    opacity: 0.5 !important; /* Här är din hover-effekt */
+  }
 `;
 
 const ProductButton = styled.button`
+  background: #ede4dd;
   width: 150px;
   padding: 10px;
+  border: 1px solid grey;
+  border-radius: 5px;
   text-transform: uppercase;
+  transition-duration: 0.6s;
+  &:hover {
+    background: #dcc6b8;
+  }
   @media (max-width: 480px) {
     padding: 7px;
   }
